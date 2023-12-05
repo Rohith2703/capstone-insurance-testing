b95718468b2b98444341a5df6e17863c0b20fdfa
Rohith2703 <134120630+Rohith2703@users.noreply.github.com>
2023-12-05 20:01:55 +0530

    Update App.java

--- src/main/java/capstone/insurancetesting/App.java
+++ src/main/java/capstone/insurancetesting/App.java
@@ -29,7 +29,7 @@ public class App
     // open url
     System.out.println("Driver opening up the url in browser");	
     WebDriver driver = new ChromeDriver(chromeOptions);
-    driver.get("http://13.233.114.71:8080/contact.html");	
+    driver.get("http://43.205.116.64:8081/contact.html");	
     //invole implicit waits to load the page
     driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     System.out.println("Enter details in the form");
